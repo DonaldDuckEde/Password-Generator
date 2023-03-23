aab6e33f4fa5bfa553888e491460ca3cbd09287a
DonaldDuckEde <119262202+DonaldDuckEde@users.noreply.github.com>
2023-03-23 18:32:15 +0100

    Update function.py

--- function.py
+++ function.py
@@ -1,13 +1,8 @@
 import random
 import string
 
-def generatePassword(length):
-    pw = []
+def generatepassword(length):
+    pw = ""
     for i in range(length):
-        pw.append(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation)
-    
-    for i in range(length):
-        temp = length - i
-        print(pw[temp])
-
-generatePassword(10)
+        pw += random.choice(string.ascii_letters + string.digits)
+    return pw
